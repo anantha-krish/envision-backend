81d28c067e02864e9488260db0b3600ebddeefea
Anantha Krishnan <anantha.krishnan@outlook.com>
2025-03-12 22:14:02 +0530

    complete file-service

---
+++ file-service/src/fileController.ts
@@ -0,0 +1,134 @@
+import { Request, Response } from "express";
+import {
+  DeleteObjectCommand,
+  DeleteObjectsCommand,
+  ListObjectsV2Command,
+} from "@aws-sdk/client-s3";
+import { AWS_S3_BUCKET_NAME } from "./config/env";
+import { uploadFilesToS3 } from "./uploadFile";
+import { getSignedFileUrl, s3Client } from "./config/s3-config";
+
+export const uploadFileHandler = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  if (!req.files || !(req.files instanceof Array)) {
+    res.status(400).json({ message: "No files uploaded!" });
+    return;
+  }
+  try {
+    const ideaId = req.params.ideaId;
+
+    if (ideaId.length === 0) {
+      throw Error("No ideaId found");
+    }
+    const fileUrls = await uploadFilesToS3(
+      ideaId,
+      req.files as Express.Multer.File[]
+    );
+    res.json({ message: "Files uploaded successfully!", fileUrls });
+  } catch (error) {
+    res.status(500).json({ error: "File upload failed!" });
+  }
+};
+
+export const deleteAttachementHandler = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const ideaId = req.params.ideaId;
+  if (!ideaId) {
+    res.status(400).json({ message: "ideaId is required" });
+    return;
+  }
+  try {
+    const data = await s3Client.send(
+      new ListObjectsV2Command({
+        Bucket: AWS_S3_BUCKET_NAME,
+        Prefix: `idea-uploads/${ideaId}`,
+      })
+    );
+
+    const filesToDelete = data.Contents
+      ? await Promise.all(
+          data.Contents.map(async ({ Key }) => ({
+            Key,
+          }))
+        )
+      : [];
+
+    const deleteCommand = new DeleteObjectsCommand({
+      Bucket: AWS_S3_BUCKET_NAME,
+      Delete: {
+        Objects: filesToDelete,
+        Quiet: false,
+      },
+    });
+
+    await s3Client.send(deleteCommand);
+
+    res.json({
+      message: `Deleted ${filesToDelete.length} files successfully.`,
+      deletedFiles: filesToDelete.map((file) => file.Key),
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteOneAttachementHandler = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const ideaId = req.params.ideaId;
+  const fileId = req.params.fileId;
+
+  if (!ideaId || !fileId) {
+    res.status(400).json({ message: "IdeaId & FileId is required" });
+    return;
+  }
+  try {
+    // Construct S3 object key based on the folder structure
+    const fileKey = `idea-uploads/${ideaId}/${fileId}`;
+
+    // Delete the file from S3
+    const deleteCommand = new DeleteObjectCommand({
+      Bucket: AWS_S3_BUCKET_NAME,
+      Key: fileKey,
+    });
+
+    await s3Client.send(deleteCommand);
+
+    res.json({ message: "File deleted successfully from S3" });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getAllAttachments = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const ideaId = req.params.ideaId ?? "";
+  try {
+    const data = await s3Client.send(
+      new ListObjectsV2Command({
+        Bucket: AWS_S3_BUCKET_NAME,
+        ...(ideaId && ideaId.length > 0
+          ? { Prefix: `idea-uploads/${ideaId}` }
+          : {}),
+      })
+    );
+    const files = data.Contents
+      ? await Promise.all(
+          data.Contents.map(async (file) => ({
+            key: file.Key,
+            url: await getSignedFileUrl(file.Key ?? ""),
+          }))
+        )
+      : [];
+    res.json({ files });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
