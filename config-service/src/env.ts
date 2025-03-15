056de84d7ced64c76ed970054f0f7b028fbd511b
Anantha Krishnan <anantha.krishnan@outlook.com>
2025-03-15 07:59:08 +0530

    remove config service

--- config-service/src/env.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const SERVER_PORT = Number(process.env.SERVER_PORT) || 4000;
-export const SERVER_HOST = process.env.SERVER_HOST || "localhost";
