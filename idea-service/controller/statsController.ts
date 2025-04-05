4c1fc35d81a3ac36095a3c80cefb5a0a11b36ae9
Anantha Krishnan <anantha.krishnan@outlook.com>
2025-04-05 13:20:44 +0530

    fixed

--- idea-service/controller/statsController.ts
+++ idea-service/controller/statsController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { startOfMonth, endOfDay, formatISO, startOfDay } from "date-fns";
 import { ideaRepo } from "../src/repo/ideasRepo";
 import { getEngagementMetrics } from "../src/kafka/engagementAsync";
+import { db } from "../src/db/db.connection";
+import { ideas, ideaTags, tags } from "../src/db/schema";
+import { count, eq, gte, and, desc, lte } from "drizzle-orm";
 
 export const getIdeaSubmissions = async (req: Request, res: Response) => {
   var currentDate = Date.now();
@@ -48,4 +51,34 @@ export const getTopIdeas = async (req: Request, res: Response) => {
   });
 
   res.status(200).json(finalResults);
+};
+
+export const getTrendingTags = async (req: Request, res: Response) => {
+  var currentDate = Date.now();
+  const startDate =
+    (req.query.startDate as string) ??
+    formatISO(startOfMonth(currentDate), { representation: "date" });
+  const endDate =
+    (req.query.endDate as string) ??
+    formatISO(currentDate, { representation: "date" });
+  const startTimestamp = startOfDay(startDate);
+  const endTimestamp = endOfDay(endDate);
+  var result = await db
+    .select({
+      tag: tags.name,
+      count: count(ideaTags.ideaId).as("count"),
+    })
+    .from(ideaTags)
+    .innerJoin(tags, eq(ideaTags.tagId, tags.id))
+    .innerJoin(ideas, eq(ideaTags.ideaId, ideas.id))
+    .where(
+      and(
+        gte(ideas.createdAt, startTimestamp),
+        lte(ideas.createdAt, endTimestamp)
+      )
+    )
+    .groupBy(tags.name)
+    .orderBy(desc(count(ideaTags.ideaId)))
+    .limit(5);
+  res.status(200).json(result);
 };
--- idea-service/routes/statsRoutes.ts
+++ idea-service/routes/statsRoutes.ts
@@ -4,12 +4,15 @@ import {
   getIdeaSubmissions,
   getTopContributors,
   getTopIdeas,
+  getTrendingTags,
 } from "../controller/statsController";
+import { get } from "http";
 const statsRouter = express.Router();
 
 statsRouter.get("/idea-submissions", getIdeaSubmissions);
 statsRouter.get("/top-ideas", getTopIdeas);
 statsRouter.get("/top-contributors", getTopContributors);
 statsRouter.get("/status-distribution", getIdeaStatusDistribution);
+statsRouter.get("/trending-tags", getTrendingTags);
 
 export default statsRouter;
